a86cbdbdfc1216603d8c8df9d668ada4b64c4acf
Winness Rakotozafy <winness.rakotozafy@outlook.fr>
2023-10-26 14:29:00 +0200

    Second commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  login : string = "toto";
+  valid : boolean = true;
+
+  isValid() : boolean {
+    return this.valid;
+  }
+
+  desactivation(){
+    return this.valid = false;
+  }
+}
+
